4a7241ffff3d721344baf8cd79a8ad3b22b34af4
trunk44 <gregory.krymov@gmail.com>
2019-09-17 23:13:34 +0300

    gind geo tags

---
+++ README.md
@@ -0,0 +1,1 @@
+# find GPS geotags in directory files
---
+++ findgeoexifs.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# packet-image-resize
+# Поиск и вывод на экран GPS геотэгов из изображений в текущей директории.
+# 
+
+
+from PIL import Image
+import glob, os
+from getch import pause
+from GPSPhoto import gpsphoto
+#import exifread
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def cls():
+    os.system('clear')
+    
+def search():
+    i = 0
+    n = 0
+
+    cls()
+    files = glob.glob(dir_path + '/**/*.*', recursive=True)
+    print('\n please wait for searching ...\n')
+    for file in files:
+        if Image: 
+            try:
+                img = Image.open(file)
+                
+                # Get the data from image file and return a dictionary
+                data = gpsphoto.getGPSData(file)
+                print('in file', file, 'GPS tag was founded: ', data['Latitude'], data['Longitude'])
+            
+#                tags = exifread.process_file(open(file, 'rb'))                                              
+#                geo = {i:tags[i] for i in tags.keys() if i.startswith('GPS')}  
+#                print('in file', file, 'find GEO ', geo)
+                
+            except:
+                pass
+
+
+def main():
+    cls()
+    print('\nfind from exif \nand print GPS tags \nfrom directory photos\n ')
+    pause('Press Any Key To continue')
+    search()
+
+main()
