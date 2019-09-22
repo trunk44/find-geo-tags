9bd1138fcfbcf31b34f31b3ef4bc68d10e34a652
trunk44 <16759830+trunk44@users.noreply.github.com>
2019-09-22 21:06:50 +0300

    Add files via upload

--- findgeotags-multi.py
+++ findgeotags-multi.py
@@ -4,7 +4,6 @@
 # Поиск и вывод на экран GPS геотэгов из изображений в текущей директории.
 # 
 
-
 from PIL import Image
 import glob, os
 from datetime import datetime
@@ -15,27 +14,19 @@ from multiprocessing import Pool
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def cls():
-    os.system('clear')
-    
 def search(file):
-
     if Image:
         try:
             img = Image.open(file)
             data = gpsphoto.getGPSData(file)
             print('in file', file, 'GPS tag was founded: ', data['Latitude'], data['Longitude']) 
         except:
-            pass   
-
-
-
-
+            pass 
+        
 if __name__ == '__main__':
     cpucores = multiprocessing.cpu_count()
     files = glob.glob(dir_path + '/**/*.*', recursive=True)
-    cls()
-    print('\n find from exif \n and print GPS tags \n from directory photos \n ')
+    print('\n print exif gps tags recirsively, \n from current directory photos \n')
     pause('Press Any Key To continue')
     startTime = datetime.now().replace(microsecond=0)
     pool = Pool(processes=cpucores + 1)
